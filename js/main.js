6a1556e60bc04b1fcb832f72177848bb8f47f3aa
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-22 14:46:54 -0700

    Create data model

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// var data = { direction: 'east' };
+// -MAKE LISTENER
+// -MAKE FUNCTION TO CHANGE DIRECTION AND UPDATE DATA
+
+// function changeDirection(event) {
+//   if (event.code === 'ArrowRight') {
+//     data.direction = 'east';
+//   } else if (event.code === 'ArrowUp') {
+//     data.direction = 'west';
+//   } else if (event.code === 'ArrowLeft') {
+//     data.direction = 'north';
+//   } else if (event.code === 'ArrowDown') {
+//     data.direction = 'south';
+//   }
+
+//   carImg.classList = 'car-img ' + data.direction;
+//   // console.log(data);
+// }
+
+// var carImg = document.querySelector('img');
+// document.addEventListener('keydown', changeDirection);
